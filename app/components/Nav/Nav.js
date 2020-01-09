95d55e8d568676d8a0826d11fd087eb82132732c
Travis Rich <travis.s.rich@gmail.com>
2020-01-09 11:48:13 +0000

    update: add link

--- app/components/Nav/Nav.js
+++ app/components/Nav/Nav.js
@@ -9,7 +9,12 @@ const NavBar = function() {
 			<Link to="/">
 				<img src="/logo.png" alt="Underlay Logo" />
 			</Link>
-			<span style={{ marginLeft: '250px' }}>Edit Text</span>
+			<a
+				href="https://docs.google.com/document/d/13KpOrtMo4cWFNrmf0Qf12G-kQ9kgN4AlLRmNSNiGdKE/edit?usp=sharing"
+				style={{ marginLeft: '250px' }}
+			>
+				Edit Text
+			</a>
 		</nav>
 	);
 };
